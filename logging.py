cb16c1e744144960800b6292cdf27afa469129f1
mukhlesur-github <mukhlesur.official@gmail.com>
2024-09-02 10:27:09 +0530

    modifying logging script

--- logging.py
+++ logging.py
@@ -1,1 +1,20 @@
 print("this is for logging")
+
+# git add file.py #single addition of file
+
+# git add .  #staging of all the changed files
+
+# git status #to check the status
+
+# git commit -m "meaningfull message" #commit the staging objects
+
+# git push origin main #upload the same into github portal
+
+#origin means the github link
+
+#main is branch
+
+
+
+
+
